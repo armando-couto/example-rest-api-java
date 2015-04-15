319146ae20513147f4a16c956221bc0c84cd1ff2
armando.couto <armand.couto@arce.ce.gov.br>
2015-04-15 14:04:19 -0300

    Concluindo exemplo.

--- src/main/java/br/com/armandocouto/controllers/CrunchifyRESTService.java
+++ src/main/java/br/com/armandocouto/controllers/CrunchifyRESTService.java
@@ -1,22 +1,25 @@
 package br.com.armandocouto.controllers;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import br.com.armandocouto.models.Track;
 
-@Path("/")
+@Path("/user")
 public class CrunchifyRESTService {
 	
 	@GET
-	@Path("/get")
+	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getTrackInJSON() {
+	public Response list() {
  
 		Track track = new Track();
 		track.setTitle("Enter Sandman");
@@ -26,14 +29,76 @@ public class CrunchifyRESTService {
  
 	}
 	
+	@GET
+	@Path("/new")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response preCreate() {
+		
+		String result = "New";
+		
+		// return HTTP response 200 in case of success
+		return Response.status(200).entity(result).build();
+	}
+	
 	@POST
-	@Path("/post")
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response student(Track track) {
+	public Response create(Track track) {
 		
 		String result = "Track OK : " + track.getSinger();
 		
 		// return HTTP response 200 in case of success
 		return Response.status(200).entity(result).build();
 	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response show(@PathParam("id") int id) {
+		
+		String result = "ID: " + id;
+		
+		// return HTTP response 200 in case of success
+		return Response.status(200).entity(result).build();
+	}
+	
+	@GET
+	@Path("/{id}/edit")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response edit(@PathParam("id") int id) {
+		
+		String result = "ID de edit: " + id;
+		
+		// return HTTP response 200 in case of success
+		return Response.status(200).entity(result).build();
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(@PathParam("id") int id, Track track) {
+		
+		String result = "Update : " + track.getSinger() + ", ID: " + id;
+		
+		// return HTTP response 200 in case of success
+		return Response.status(200).entity(result).build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response destroy(@PathParam("id") int id) {
+		
+		String result = "ID destroy: " + id;
+		
+		// return HTTP response 200 in case of success
+		return Response.status(200).entity(result).build();
+	}
+	
+	
 }
